01a5b5e24f63be52736fc9a75a458d439b21bc34
hesam21188 <hosseinharatii@gmail.com>
2024-10-25 08:30:56 +0330

    ui

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class PromptForm(forms.Form):
-    prompt = forms.CharField(label="",max_length=256)
+    prompt = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'مشخصات ماشینی که میخوای رو بنویس', 'class':'form-control','style':'line-height: 2.5'}), label="",max_length=256)
